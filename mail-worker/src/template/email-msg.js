b9365d791e6cfd1bdbebfe0a6cf1d1ce73d811a6
ngapngap <27039619+ngapngap@users.noreply.github.com>
2026-01-05 13:50:17 +0700

    Sửa lỗi syntax trong email-msg.js

--- mail-worker/src/template/email-msg.js
+++ mail-worker/src/template/email-msg.js
@@ -1,37 +1,22 @@
 import emailUtils from '../utils/email-utils';
-
 export default function emailMsgTemplate(email, tgMsgTo, tgMsgFrom, tgMsgText) {
-
-	let template = `<b>${email.subject}</b>`
-
-		if (tgMsgFrom === 'only-name') {
-			template += `
-
-template += `<br>Người gửi: ${email.name}`		}
-
-		if (tgMsgFrom === 'show') {
-			template += `
-
-template += `<br>Người gửi: ${email.name} <${email.sendEmail}>`		}
-
-		if(tgMsgTo === 'show' && tgMsgFrom === 'hide') {
-			template += `
-
-template += `<br>Người nhận: \u200B${email.toEmail}`
-		} else if(tgMsgTo === 'show') {
-		template += `
-template += `<br>Người nhận: \u200B${email.toEmail}`	}
-
-	const text = (email.text || emailUtils.htmlToText(email.content))
-		.replace(/</g, '&lt;')
-		.replace(/>/g, '&gt;');
-
-	if(tgMsgText === 'show') {
-		template += `
-
-${text}`
-	}
-
-	return template;
-
+  let template = `<b>${email.subject}</b>`
+  if (tgMsgFrom === 'only-name') {
+    template += `<br>Người gửi: ${email.name}`
+  }
+  if (tgMsgFrom === 'show') {
+    template += `<br>Người gửi: ${email.name} <${email.sendEmail}>`
+  }
+  if(tgMsgTo === 'show' && tgMsgFrom === 'hide') {
+    template += `<br>Người nhận: \u200B${email.toEmail}`
+  } else if(tgMsgTo === 'show') {
+    template += `<br>Người nhận: \u200B${email.toEmail}`
+  }
+  const text = (email.text || emailUtils.htmlToText(email.content))
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;');
+  if(tgMsgText === 'show') {
+    template += `<br>${text}`
+  }
+  return template;
 }
